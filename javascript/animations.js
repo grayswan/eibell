25f6e9945894dbd7a0a0e05443685ece0dd320e7
Kristen Grashel <kristen@spatialnetworks.com>
2017-10-08 00:14:38 -0400

    Clean up

--- javascript/animations.js
+++ javascript/animations.js
@@ -1,3 +1,21 @@
+var isOnScreen = function() {
+  var win = $(window);
+
+  var viewport = {
+      top : win.scrollTop(),
+      left : win.scrollLeft()
+  };
+  viewport.right = viewport.left + win.width();
+  viewport.bottom = viewport.top + win.height();
+
+  var bounds = win.offset();
+  bounds.right = bounds.left + this.outerWidth();
+  bounds.bottom = bounds.top + this.outerHeight();
+
+  return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+};
+
+
 $( document ).ready(function() {
   var cssSelector = anime({
     targets: '#animated-text-container .el',
@@ -8,12 +26,31 @@ $( document ).ready(function() {
     duration: 3000
   });
 
-  var cssSelector = anime({
-    targets: '#animated-text-container .el',
-    translateY: [30, 30],
-    easing: 'easeInOutQuad',
-    opacity: [0, 1],
-    delay: 100,
-    duration: 3000
-  });
+  var basicTimeline = anime.timeline();
+
+  basicTimeline
+    .add({
+      opacity: 1,
+      targets: '#services .services.el',
+      translateY: -50,
+      easing: 'easeOutExpo',
+      duration: 500,
+      delay: 100
+    })
+    .add({
+      opacity: 1,
+      targets: '#services .repair.el',
+      translateY: -50,
+      easing: 'easeOutExpo',
+      duration: 500,
+      delay: 100
+    })
+    .add({
+      opacity: 1,
+      targets: '#services .performance.el',
+      translateY: -50,
+      easing: 'easeOutExpo',
+      duration: 500,
+      delay: 100
+    })
 });
